632287dd020115ee2c11b3b4ea0520d159bdc096
liudonghua123 <liudonghua123@gmail.com>
2021-04-07 10:40:16 +0800

    initial code generated via `stagehand console-full`

---
+++ bin/generate_named_acl.dart
@@ -0,0 +1,5 @@
+import 'package:generate_named_acl/generate_named_acl.dart' as generate_named_acl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${generate_named_acl.calculate()}!');
+}
---
+++ test/generate_named_acl_test.dart
@@ -0,0 +1,8 @@
+import 'package:generate_named_acl/generate_named_acl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
